169d5ed7d3641a8757d91d27ba987c3f18380c34
Carlos <carlos.delvalle@factorlibre.com>
2021-04-20 15:33:37 +0000

    14.0-ADD_helpdesk_carlosvalle

--- helpdesk_carlosvalle/models/helpdesk_ticket.py
+++ helpdesk_carlosvalle/models/helpdesk_ticket.py
@@ -1,9 +1,51 @@
 from odoo import fields, models 
 
+class HelpdeskTicketAction(models.Model):
+    _name = 'helpdesk.ticket.action'
+    _description = 'Action'
+
+    name = fields.Char()
+    date = fields.Date()
+    ticket_id = fields.Many2one(
+        comodel_name='helpdesk.ticket',
+        string='Ticket')
+
+
+class HelpdeskTicketTag(models.Model):
+    _name = 'helpdesk.ticket.tag'
+    _description = 'Tag'
+
+    name = fields.Char()
+    tickets_ids = fields.Many2many(
+        comodel_name='helpdesk.ticket',
+        relation='helpdesk_ticket_tag_rel', # Como quiero que se llame la tabla intermedia
+        column1='tag_id',                   #
+        column2='ticket_id',                #
+        string='Tags')
+
+
 class HelpdeskTicket(models.Model):
     # nombre de la tabla en la bbdd
     _name = 'helpdesk.ticket'
     _description = 'Ticket'
+
+
+
+    tag_ids = fields.Many2many(
+        comodel_name='helpdesk.ticket.tag',
+        relation='helpdesk_ticket_tag_rel', # Como quiero que se llame la tabla intermedia
+        column1='ticket_id',                #
+        column2='tag_id',                   #
+        string='Tags')
+
+
+    # Esto apuntan al ticket 
+    action_ids = fields.One2many(
+        comodel_name='helpdesk.ticket.action',
+        inverse_name='ticket_id',
+        string='Actions')
+
+
 
     # string es para el nombre de la vista en odoo.
     # El nombre de la variable es para bbdd
@@ -31,7 +73,7 @@ class HelpdeskTicket(models.Model):
 
     assigned = fields.Boolean(
         string='Assigned',
-        readonly=True)
+        compute='_compute_assigned')
 
     date_limit = fields.Date(
         string='Date Limit')
@@ -43,6 +85,13 @@ class HelpdeskTicket(models.Model):
     action_preventive = fields.Html(
         string='Preventive Action',
         help='Descrive preventive actions to do')
+
+
+    user_id = fields.Many2one(      # Habra una columna que sea user id y apunte al otro id 
+        comodel_name='res.users',
+        string='Assigned to'
+    )
+
 
     
     def asignar(self):
@@ -71,4 +120,10 @@ class HelpdeskTicket(models.Model):
     def cancelar(self):
         self.ensure_one()
         self.state = 'cancelado'
-        
+
+   #@api.depends('user_id')  
+    def _compute_assigned(self):
+        for record in self:
+            record.assigned = self.user_id and True or False
+    
+
